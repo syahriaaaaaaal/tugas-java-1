36e98ab4afd55000b0195f77d17d82d88d6e6db0
syahriaaaaaaal <129356684+syahriaaaaaaal@users.noreply.github.com>
2024-08-08 15:01:27 +0700

    Add files via upload

---
+++ nugas/src/nugas/Nugas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nugas;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Nugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
