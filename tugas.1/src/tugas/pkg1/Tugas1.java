e46a1fdfc529a4d123cbd875a30642b84d0a472c
syahriaaaaaaal <129356684+syahriaaaaaaal@users.noreply.github.com>
2024-08-08 14:34:57 +0700

    Add files via upload

---
+++ tugas.1/src/tugas/pkg1/Tugas1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas.pkg1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        modul1 modul = new modul1();
+        modul.setVisible(true);
+    }
+    
+}
